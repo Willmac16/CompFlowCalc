592b3ed4146efee6d3b3c2f9e00c2c89da0eeb88
Will MacCormack <willmaccormack@gmail.com>
2024-04-04 18:24:22 -0400

    HW 9

--- __init__.py
+++ __init__.py
@@ -2,6 +2,7 @@ import math
 from scipy.optimize import root_scalar
 import numpy as np
 
+# Stag over static
 class IsentropicRatio:
     def __init__(self, gamma, mach):
         self.temp_ratio = 1 + (gamma - 1) / 2 * mach ** 2
@@ -75,20 +76,54 @@ class ThetaBetaMach:
                                     x0=7/9 * math.pi / 4,
                                     bracket=[1e-6, math.pi / 4],
                                     method='brentq').root
+    def __str__(self):
+        return f"Theta: {self.theta:4f}\nBeta:  {self.beta:4f}\nMach:  {self.mach:4f}"
 
-class ObliqueShockResult:
-    def __init__(self, gamma, mach_one, beta):
+    def __repr__(self):
+        return self.__str__()
+
+class ObliqueShock:
+    def __init__(self, gamma, mach=math.nan, beta=math.nan, theta=math.nan):
+        self.tbm = ThetaBetaMach(gamma, mach, beta, theta)
+        mach_one = self.tbm.mach
+        beta = self.tbm.beta
+        theta = self.tbm.theta
+
         self.theta = math.atan(2 / math.tan(beta) * (mach_one ** 2 * math.sin(beta) ** 2 - 1) / (mach_one ** 2 * (gamma + math.cos(2 * beta)) + 2))
         mach_one_normal = mach_one * math.sin(beta)
 
         self.shock_ratio: NormalShockRatio = NormalShockRatio(gamma, mach_one_normal)
         mach_two_normal = self.shock_ratio.mach_two
-        self.mach_two = mach_two_normal / math.sin(self.theta - beta)
+        self.mach_two = mach_two_normal / math.sin(theta - beta)
 
+
+def prandtlMeyerNu(gamma, mach):
+    return math.sqrt((gamma + 1) / (gamma - 1)) * math.atan(math.sqrt((gamma - 1) / (gamma + 1) * (mach ** 2 - 1))) - math.atan(math.sqrt(mach ** 2 - 1))
 
 class PrandtlMeyer:
-    def __init__(self, gamma, mach):
-        self.nu = math.sqrt((gamma + 1) / (gamma - 1)) * math.atan(math.sqrt((gamma - 1) / (gamma + 1) * (mach ** 2 - 1))) - math.atan(math.sqrt(mach ** 2 - 1))
+    def __init__(self, gamma, mach_one=math.nan, mach_two=math.nan, theta=math.nan):
+        if not math.isnan(mach_one) and not math.isnan(mach_two):
+            self.mach_one = mach_one
+            self.mach_two = mach_two
+            self.theta = prandtlMeyerNu(gamma, mach_two) - prandtlMeyerNu(gamma, mach_one)
+        elif not math.isnan(mach_one) and not math.isnan(theta):
+            self.mach_one = mach_one
+            self.theta = theta
+            self.mach_two = root_scalar(lambda mach_two:
+                                        prandtlMeyerNu(gamma, mach_two) - prandtlMeyerNu(gamma, mach_one) - theta,
+                                        bracket=[mach_one, 1000]).root
+        elif not math.isnan(mach_two) and not math.isnan(theta):
+            self.mach_two = mach_two
+            self.theta = theta
+            self.mach_one = root_scalar(lambda mach_one:
+                                        prandtlMeyerNu(gamma, mach_two) - prandtlMeyerNu(gamma, mach_one) - theta,
+                                        bracket=[1e-6, mach_two]).root
+
+    def __str__(self):
+        return f"Mach 1: {self.mach_one:4f}\nMach 2: {self.mach_two:4f}\nTheta:  {self.theta:4f}"
+
+    def __repr__(self):
+        return self.__str__()
 
 if __name__ == "__main__":
     GAMMA = 1.4
